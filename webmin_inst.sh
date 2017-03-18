bc9de499f538d338af92b86dfac4f7ad75968397
Andre Mattie <devel@introsec.ca>
2017-03-18 15:43:59 -0400

    added variables and initial commands
    Signed-off-by: Andre Mattie <devel@introsec.ca>

--- webmin_inst.sh
+++ webmin_inst.sh
@@ -13,11 +13,13 @@
 ################################################################################
 
 # variables
-ROOTUID="0"
 PORT=$1
+DEPENDS="perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python apt-transport-https"
+REPO="deb http://download.webmin.com/download/repository sarge contrib"
+KEY="jcameron-key.asc"
 
 # ensure script is being run by root
-if [ "$(id -u)" -ne "$ROOTUID" ] ; then
+if [ "$(id -u)" -ne "0" ] ; then
     echo "You must be root to run this script"
     exit 1
 fi
@@ -27,10 +29,25 @@ fi
 # update system
 apt-get update && apt-get upgrade -y
 
-#TODO add Webmin repo to /etc/sources.list.d
+# mv to tmp directory
+cd /tmp
 
-#TODO install dependencies
+# download Webmin repository key
+wget  http://www.webmin.com/$KEY
 
-#TODO install webmin
+# add repository key to apt
+apt-key add $KEY
+
+# create file for Webmin repository
+touch /etc/apt/sources.list.d/webmin.list
+
+# add Webmin repo to /etc/sources.list.d
+echo $REPO >> /etc/apt/sources.list.d/webmin.list
+
+# install dependencies
+apt-get install -y $DEPENDS
+
+# install webmin
+apt-get install -y webmin
 
 #TODO set port number from arguments
